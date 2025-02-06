541695f25aeaf9c25b167c150de0893363abc868
YannickZahinda <ymulikuza@gmail.com>
2025-02-06 05:05:02 +0200

    add about component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,14 +1,53 @@
-import React from 'react'
+// import eventPlanImage from '../assets/generated-image-preview.png';
+import celebrateImage from "../assets/aboutimg.png";
+import Button from "../components/Button/Button";
 
 const About = () => {
   return (
-    <div>
-      <h1>A propos</h1>
-      <div>
-        
-      </div>
-    </div>
-  )
-}
+    <section className="mt-15">
+      <h1 className="text-center text-5xl font-serif font-bold">A propos</h1>
 
-export default About
+      <div className="flex items-center gap-4 text-center justify-center mt-4">
+        <span className="block w-16 h-[1px] bg-gray-400"></span>
+        <h1 className="text-2xl font-serif font-bold tracking-wide text-[#4A1316]">
+          NOTRE ORGANISATION
+        </h1>
+        <span className="block w-16 h-[1px] bg-gray-400"></span>
+      </div>
+
+      <div className="flex items-center justify-center gap-8 p-8">
+        <img src={celebrateImage} alt="planning pic"
+          className="w-2/3 max-w-2xl"
+        />
+        <div className="flex flex-col gap-6 w-1/2 text-justify">
+          <p className="font-bold text-3xl">
+            Nous sommes une équipe des passionnés et experimentés en
+            événementiels
+          </p>
+          <p className="text-lg leading-relaxed">
+            Myrja Events a été fondée en 2021 par ABUS’ALANGE BILUGE Myrja, une
+            passionnée de l'événementiel avec une vision claire : créer des
+            expériences mémorables et uniques pour chaque occasion. Dès ses
+            débuts, l'agence a misé sur la créativité, l'excellence et une
+            approche personnalisée pour se démarquer dans l'industrie
+            événementielle. Au fil des années, Myrja Events a organisé avec
+            succès une variété d'événements, allant des mariages somptueux aux
+            conférences professionnelles en passant par des fêtes d'entreprise
+            et des célébrations privées. Chaque projet a permis à l'agence de
+            renforcer sa réputation d'excellence et de devenir un leader
+            respecté dans le domaine de l'événementiel. Aujourd'hui, Myrja
+            Events continue de croître, en restant fidèle à ses valeurs
+            fondamentales et en s'engageant à offrir des services de premier
+            ordre à ses clients.
+          </p>
+          <Button
+            className="px-4 w-1/4 py-3 text-gray-800 border-2 border-[#4A1316] rounded-3xl hover:bg-[#4A1316] hover:text-white transition duration-300"
+            text="Reservez dès maintenant "
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,24 @@
-// import eventPlanImage from '../assets/generated-image-preview.png';
-// import celebrateImage from '../assets/Allura - Proposal.png'
 import Button from "../components/Button/Button";
 import Navbar from "../components/Navbar.tsx/Navbar";
+import About from "./About";
 import Services from "./Services";
 
 const Home = () => {
   return (
+    <>
     <div className="relative h-screen w-full bg-[url('/assets/niceweddingimg.jpg')] bg-cover bg-center">
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="relative">
         <Navbar />
       </div>
       {/* Hero Section */}
-      <section className="flex items-center justify-center text-white text-2xl h-full">
+      <section className="flex items-center justify-center text-white text-2xl h-screen">
         <div className="relative text-center mt-3">
           <div className="flex items-center justify-center m-7">
             <div className="flex items-center gap-4 text-center">
               <span className="block w-16 h-[1px] bg-gray-400"></span>
               <h1 className="text-2xl font-serif tracking-wide text-white">
-                WEDDING PLANNER
+                EVENTS PLANNER
               </h1>
               <span className="block w-16 h-[1px] bg-gray-400"></span>
             </div>
@@ -32,7 +32,10 @@ const Home = () => {
       </section>
       {/* Service section */}
       {/* <Services /> */}
+     
     </div>
+    <About />
+    </>
   );
 };
 
