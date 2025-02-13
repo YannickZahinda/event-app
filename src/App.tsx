81b05de5fd97e8aeeeb6fde3a2c570d32160659d
YannickZahinda <ymulikuza@gmail.com>
2025-02-13 02:51:26 +0200

    remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/Navbar.tsx/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Communication from './pages/Communication';
