41d543030e1555a64926b2cdc1867bd67d48fafd
YannickZahinda <ymulikuza@gmail.com>
2025-02-13 03:01:15 +0200

    install swipper to avoid error in deployment and fixed package.json file building scripts

--- src/components/slides/ServicesList.tsx
+++ src/components/slides/ServicesList.tsx
@@ -4,7 +4,6 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import servicesData from "../../data/servicesData";
-import Button from "../Button/Button";
 import ReactWhatsapp from "react-whatsapp";
 
 
